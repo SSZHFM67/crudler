e0b220d051d05add220d5d9895ae35defc74bdf3
SSZHFM67 <mustaphadaudi@icloud.com>
2025-12-09 09:50:10 +0000

    Updates to Favourite.js
    Allow user to favourite with a tap rather than a long press.

--- src/screens/components/UI/Favourite.js
+++ src/screens/components/UI/Favourite.js
@@ -2,3 +2,19 @@ import React from 'react';
 import { StyleSheet, Text } from 'react-native';
 import Selector from './Selector';
 import Icons from './Icons';
+
+const Favourite = ({ isFavourite, onPress, style }) => {
+    const icon = isFavourite ? Icons.Favourite() : Icons.NotFavourite();
+
+  return (
+    <Selector
+      onPress={onPress}
+      style={[styles.container, style]}
+      pressedStyle={styles.pressed}
+      // tap only 
+      useLongPress={false}
+    >
+      <Text style={styles.icon}>{icon}</Text>
+    </Selector>
+  );
+};
