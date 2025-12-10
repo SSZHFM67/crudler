0990ed1d6d611dcdc9ea83da9b2c45c43adab915
SSZHFM67 <mustaphadaudi@icloud.com>
2025-12-10 07:45:28 +0000

    Create users.js
    Added sample data for file to be replaced later with database data from site

---
+++ src/screens/data/users.js
@@ -0,0 +1,24 @@
+const initialUsers = [
+  {
+    UserID: 1,
+    UserName: 'Graeme Jones',
+    UserRole: 'Staff',
+  },
+  {
+    UserID: 2,
+    UserName: 'Paul Knave',
+    UserRole: 'Staff',
+  },
+  {
+    UserID: 3,
+    UserName: 'Justin TWUMASI',
+    UserRole: 'Student',
+  },
+  {
+    UserID: 4,
+    UserName: 'Luke STEVENS',
+    UserRole: 'Student',
+  },
+];
+
+export default initialUsers;
