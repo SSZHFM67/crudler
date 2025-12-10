65b9c85752975f38859eec580ecfa333f8c61c17
SSZHFM67 <mustaphadaudi@icloud.com>
2025-12-10 08:19:57 +0000

    Update App.js
    Added User CRUDL stack and integrate into drawer

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { LogBox, TouchableOpacity, Text, View } from 'react-native';
+import { LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
@@ -9,12 +9,114 @@ import ModuleAddScreen from './src/screens/ModuleAddScreen';
 import ModuleViewScreen from './src/screens/ModuleViewScreen';
 import ModuleModifyScreen from './src/screens/ModuleModifyScreen';
 
-// Ignore the non-serializable warning
+import UserListScreen from './src/screens/UserListScreen';
+import UserAddScreen from './src/screens/UserAddScreen';
+import UserViewScreen from './src/screens/UserViewScreen';
+import UserModifyScreen from './src/screens/UserModifyScreen';
+
 LogBox.ignoreLogs([
   'Non-serializable values were found in the navigation state',
 ]);
 
-const ModulesStack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function ModulesStackNavigator() {
+  return (
+    <Stack.Navigator
+      initialRouteName="ModuleList"
+      screenOptions={{
+        headerStyle: { backgroundColor: '#000' },
+        headerTintColor: '#fff',
+        headerTitleStyle: { fontWeight: '600' },
+      }}
+    >
+      <Stack.Screen
+        name="ModuleList"
+        component={ModuleListScreen}
+        options={{ title: 'Module Crudler' }}
+      />
+      <Stack.Screen
+        name="ModuleAdd"
+        component={ModuleAddScreen}
+        options={{ title: 'Add Module' }}
+      />
+      <Stack.Screen
+        name="ModuleView"
+        component={ModuleViewScreen}
+        options={{ title: 'View Module' }}
+      />
+      <Stack.Screen
+        name="ModuleModify"
+        component={ModuleModifyScreen}
+        options={{ title: 'Modify Module' }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function UsersStackNavigator() {
+  return (
+    <Stack.Navigator
+      initialRouteName="UserList"
+      screenOptions={{
+        headerStyle: { backgroundColor: '#000' },
+        headerTintColor: '#fff',
+        headerTitleStyle: { fontWeight: '600' },
+      }}
+    >
+      <Stack.Screen
+        name="UserList"
+        component={UserListScreen}
+        options={{ title: 'User Crudler' }}
+      />
+      <Stack.Screen
+        name="UserAdd"
+        component={UserAddScreen}
+        options={{ title: 'Add User' }}
+      />
+      <Stack.Screen
+        name="UserView"
+        component={UserViewScreen}
+        options={{ title: 'View User' }}
+      />
+      <Stack.Screen
+        name="UserModify"
+        component={UserModifyScreen}
+        options={{ title: 'Modify User' }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName="ModulesCrudler"
+        screenOptions={{
+          headerStyle: { backgroundColor: '#000' },
+          headerTintColor: '#fff',
+          headerTitleStyle: { fontWeight: '600' },
+        }}
+      >
+        <Drawer.Screen
+          name="ModulesCrudler"
+          component={ModulesStackNavigator}
+          options={{ title: 'Module Crudler' }}
+        />
+        <Drawer.Screen
+          name="UsersCrudler"
+          component={UsersStackNavigator}
+          options={{ title: 'User Crudler' }}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+/*const ModulesStack = createNativeStackNavigator();
 function ModulesStackScreen() {
   return (
     <ModulesStack.Navigator
